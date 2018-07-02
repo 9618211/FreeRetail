afd64cc3e7277d1012a3745fddf92f80e1b290bd
zweizhao <zweizhao@foxmail.com>
2018-07-02 16:41:16 +0800

    首页前往卖家页面,卖家目前显示内容与买家一模一样

--- src/pages/seller/index.vue
+++ src/pages/seller/index.vue
@@ -1,16 +1,124 @@
 <template lang='pug'>
 .container
-  span seller
+  search(@clickSearch="clickSearch")
+  swiper(
+    :indicator-dots="bannerConfig.showDots"
+    :autoplay="bannerConfig.autoplay"
+    :interval="bannerConfig.interval"
+    :duration="bannerConfig.duration"
+    :circular="bannerConfig.circular"
+  )
+    block(
+      v-for="(url, index) of bannerConfig.urls" :key="index"
+    )
+      swiper-item
+        img.banner(
+          :src="url"
+          mode="aspectFill"
+          width="355"
+          height="150")
+  .types
+    type(
+      v-for="(type, index) of types" :key="index"
+      :type="type"
+      :index="index"
+      @clickType="clickType")
+  .current-type-block
+    .current-red
+    .current-type {{currentType}}
+  .goods-block(v-if="allGoods.length > 0")
+    goods(
+      v-for="(goods, index) of allGoods" :key="index"
+      :goods="goods")
+  .nothing(wx:else)
+    icon(type="warn" size="20")
+    span.nothing-text 什么都没有...
+  .bottom
+    span(
+      @click="callPhone"
+    ) 联系号码：
+      span.phone-number {{mobile}}
 </template>
 
 <script>
 import main from './main.coffee'
+import goods from '@/components/goods/index'
+import search from '@/components/search/index'
+import type from '@/components/type/index'
 
 export default {
-  ...main,
+  components: {
+    goods,
+    search,
+    type,
+  },
+	...main,
 }
 </script>
 
-<style lang='sass' scoped>
+<style scoped lang='sass'>
+.container
+  height: calc(100% - 80rpx)
+  overflow-y: scroll
 
+  swiper
+    width: 100%
+
+  .banner
+    width: 100%
+    height: 320rpx
+
+  .types
+    display: flex
+    flex-wrap: wrap
+    justify-content: space-around
+
+  .current-type-block
+    display: flex
+    align-items: center
+    width: 100%
+    height: 48rpx
+
+  .current-red
+    width: 16rpx
+    height: 40rpx
+    margin: 0 16rpx 0 32rpx
+    background: rgb(231, 74, 57)
+
+  .current-type
+    font-size: 28rpx
+
+  .goods-block
+    display: flex
+    flex-wrap: wrap
+    justify-content: space-between
+    align-content: flex-start
+    width: 100%
+    padding: 0 32rpx
+    margin-bottom: 80rpx
+
+  .nothing
+    display: flex
+    align-items: center
+    margin: 32rpx 0 32rpx 32rpx
+    width: 100%
+
+  .nothing-text
+    font-size: 28rpx
+    color: #666
+
+  .bottom
+    position: fixed
+    bottom: 0
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 100%
+    height: 80rpx
+    font-size: 28rpx
+    color: white
+    background: #e74a39
+
+  .phone-number
+    text-decoration: underline
 </style>
--- src/pages/seller/main.coffee
+++ src/pages/seller/main.coffee
@@ -1,7 +1,78 @@
+import banner from '@/assets/banner.png'
 import main from '@/mixins/main.coffee'
+
+Bmob = require '@/utils/Bmob.js'
+owner = ''
 
 export default
   mixins: [main]
 
   data:
-    a: '1'
+    allGoods: [] # 所有商品
+    types: ['全部', '海鲜', '火锅料', '猪', '牛羊', '鸡', '鸭', '其他美食']
+    currentType: '全部'
+    bannerConfig:
+      urls: [
+        banner
+        "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
+      ]
+      circular: true
+      showDots: true
+      interval: 3000
+      duration: 300
+      autoplay: true
+    mobile: ''
+
+  mounted: ->
+    this.query()
+
+  onLoad: (option) ->
+    owner = option.owner
+    this.mobile = option.mobile
+
+  methods:
+    query: (value) ->
+      query = Bmob.Query 'Goods'
+      query.equalTo "owner", '==', owner
+      currentType = this.currentType
+      query.equalTo "type", '==', currentType if currentType isnt '全部'
+
+      wx.showLoading title: '搜索中...'
+
+      query.find()
+        .then (results) =>
+          wx.hideLoading null
+          allGoods = []
+          for result in results
+            name = result.name
+            if value?
+              if name.includes value
+                allGoods.push
+                  url: result.url
+                  price: '¥' + result.price
+                  name: name
+              else
+                continue
+            else
+              allGoods.push
+                url: result.url
+                price: '¥' + result.price
+                name: name
+          this.allGoods = allGoods
+
+        .catch (error) ->
+          wx.hideLoading()
+          console.log error
+
+    clickType:(payload) ->
+      this.currentType = payload.type
+      this.query()
+
+    clickSearch: (payload) ->
+      value = payload.searchValue
+      this.query value
+      this.currentType = value
+
+    callPhone: ->
+      wx.makePhoneCall
+        phoneNumber: '18888888888'
